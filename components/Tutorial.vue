a9c986d5a629d5807a9f012b6fa5e68d545cbe7a
Santiago397 <santilopez397@gmail.com>
2022-10-06 14:07:10 -0500

    index

---
+++ components/Tutorial.vue
@@ -0,0 +1,16 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div>
+    <nuxt-img
+      src="wallpaper.jpg"
+      format="webp"
+      preset="products"
+      />
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
